e6dfceef80b634f69f04b566536f6fd3efe8c521
Yarik123121 <wertehuid@gmail.com>
2024-01-06 14:37:19 +0300

    Добавьте файлы проекта.

---
+++ prob2/prob2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int a, b;
+
+		std::cout << "В ведите число: ";
+		std::cin >> a;
+		std::cout << "В ведите число: ";
+		std::cin >> b;
+	
+	
+	int num = - 45;
+	std::cout << num << std::endl;
+	num = 10;
+	std::cout << num <<std::endl;
+	num += 5;
+	std::cout << num << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+}
+
+
